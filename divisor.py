fd4f2ed290323558895ac33c2f6b9ed0af0b649e
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-04-15 18:12:42 -0700

    bug fix in loop range

--- divisor.py
+++ divisor.py
@@ -1,6 +1,6 @@
 x = int(input("Please enter an integer for which you want to know the divisors: "))
 print("The divisors of the integer you entered are: ")
 
-for i in range(1, x):
+for i in range(1, x+1):
 	if(x%i == 0):
 		print(i)
