8fc57f11e6b6ee728516eb79bce1b700fd673426
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-04-15 18:11:19 -0700

    for loop

--- divisor.py
+++ divisor.py
@@ -1,2 +1,6 @@
 x = int(input("Please enter an integer for which you want to know the divisors: "))
 print("The divisors of the integer you entered are: ")
+
+for i in range(1, x):
+	if(x%i == 0):
+		print(i)
