e5871ec151ab650393736da9eb33c992551e689e
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-04-15 18:25:45 -0700

    char set up

--- password.py
+++ password.py
@@ -3,4 +3,6 @@ import string
 
 x = int(input("Please enter the length in characters of the password you want to generate: "))
 
+pw_chars = string.ascii_letters + string.punctuation + string.digits
+
 
